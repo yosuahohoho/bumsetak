c6b15d7ae184228f11d95d1cb33fa3029b54cd0d
yosuahohoho <yosuahohoho@gmail.com>
2019-08-28 13:59:44 +0700

    Add Keypad component

---
+++ src/components/Keypad.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Keypad = () => {
+  return (
+    <div className='keypad'>
+      <kbd>A</kbd>
+      <audio src='#' />
+    </div>
+  )
+}
+
+export default Keypad
