0af5c40bdb6ccb17732ad8dcf0aed678276b5772
yosuahohoho <yosuahohoho@gmail.com>
2019-08-28 14:33:44 +0700

    Check sound

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const Keypad = () => {
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    const audio = document.querySelector('audio')
+    audio.play()
+  }
+
   return (
     <div className='keypad'>
-      <button>A</button>
-      <audio src='#' />
+      <button onClick={handleClick}>A</button>
+      <audio src='https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' />
     </div>
   )
 }
