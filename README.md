51223a7dcb36fb8ac008d09922c733f5621eb492
yosuahohoho <yosuahohoho@gmail.com>
2019-08-28 16:00:52 +0700

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# BumSetak
-Drum machine for +62 people.
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.46d1861a3436672f6167beff9f59f6a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064f95f01e038134647944f24f9155f6",
+    "url": "/bumsetak/index.html"
+  },
+  {
+    "revision": "c91cb72bc64949ab0cff",
+    "url": "/bumsetak/static/css/main.82a7402b.chunk.css"
+  },
+  {
+    "revision": "6dbbd70c9374a1426bde",
+    "url": "/bumsetak/static/js/2.d6da33fd.chunk.js"
+  },
+  {
+    "revision": "c91cb72bc64949ab0cff",
+    "url": "/bumsetak/static/js/main.c5769605.chunk.js"
+  },
+  {
+    "revision": "f06b7232ea5dbe417180",
+    "url": "/bumsetak/static/js/runtime~main.3a31b787.js"
+  }
+]);
