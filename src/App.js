dc662fd5bac1c28254098b561cbcf64c6dd76932
yosuahohoho <yosuahohoho@gmail.com>
2019-08-28 14:16:32 +0700

    Change some style rules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import './App.css'
+
+import Board from "./components/Board"
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>BumSetak</header>
+      <header className='App-header'><h1>BumSetak</h1></header>
+      <Board />
       <footer>2019&copy;yosuahohoho</footer>
     </div>
   )
