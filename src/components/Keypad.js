435b07768a02f52e0915945a007ecd93de901aff
yosuahohoho <yosuahohoho@gmail.com>
2019-08-29 19:11:09 +0700

    Add handle keydown functionality using useEffect

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,20 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 
-const Keypad = (props) => {
-
+const Keypad = props => {
   const audioRef = useRef(null)
 
-  const handleClick = (e) => {
+  useEffect(() => {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === props.bank.keyCode) {
+        playSound()
+      }
+    })
+
+    return document.removeEventListener('keydown', playSound)
+  }, [props.bank.keyCode])
+
+  const handleClick = e => {
     e.preventDefault()
-    
+    playSound()
+  }
+
+  const playSound = () => {
     audioRef.current.currentTime = 0
     audioRef.current.play()
   }
 
   return (
     <div className='pad'>
-      <audio ref={audioRef} src={props.bank.url} id={props.bank.keyTrigger} />
-      <button onClick={handleClick}>{ props.bank.keyTrigger }</button>
+      <audio ref={audioRef} src={props.bank.url} id={props.bank.keyCode} />
+      <button onClick={handleClick}>{props.bank.keyTrigger}</button>
       <span>{props.bank.id}</span>
     </div>
   )
