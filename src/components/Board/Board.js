53066bcfa6870039877e2ebab9d4f9857497bfd8
yosuahohoho <yosuahohoho@gmail.com>
2019-09-04 12:24:36 +0700

    Change: move audios data to audiosData.js

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,73 +2,12 @@ import React from 'react'
 
 import Keypad from '../Keypad/Keypad'
 import styles from './board.module.css'
+import AUDIOS from './audiosData'
 
 const Board = () => {
+  let keypads = AUDIOS.map(audio => <Keypad audio={audio} key={audio.id} />)
 
-  const AUDIOS = [
-    {
-      keyCode: 81,
-      keyTrigger: 'Q',
-      id: 'Heater-1',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-    },
-    {
-      keyCode: 87,
-      keyTrigger: 'W',
-      id: 'Heater-2',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-    },
-    {
-      keyCode: 69,
-      keyTrigger: 'E',
-      id: 'Heater-3',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-    },
-    {
-      keyCode: 65,
-      keyTrigger: 'A',
-      id: 'Heater-4',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-    },
-    {
-      keyCode: 83,
-      keyTrigger: 'S',
-      id: 'Clap',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-    },
-    {
-      keyCode: 68,
-      keyTrigger: 'D',
-      id: 'Open-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-    },
-    {
-      keyCode: 90,
-      keyTrigger: 'Z',
-      id: "Kick-n'-Hat",
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-    },
-    {
-      keyCode: 88,
-      keyTrigger: 'X',
-      id: 'Kick',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-    },
-    {
-      keyCode: 67,
-      keyTrigger: 'C',
-      id: 'Closed-HH',
-      url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-    }
-  ]
-  
-  let keypads = AUDIOS.map(audio => <Keypad audio={ audio } key={ audio.id } />)
-
-  return (
-    <div className={ styles.board }>
-      { keypads }
-    </div>
-  )
+  return <div className={styles.board}>{keypads}</div>
 }
 
 export default Board
